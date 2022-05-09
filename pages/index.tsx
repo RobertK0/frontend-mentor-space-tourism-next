4fa4b36928cb1ff42c81208e1f52d0a2b21a5884
Zero0k <robykaurin@gmail.com>
2022-05-09 21:49:34 +0200

    Home page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,39 +16,60 @@ const Home: NextPage = () => {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Bellefair&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Barlow&family=Barlow+Condensed&family=Bellefair&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <main className={styles.main}>
-        <header className={styles.header}>
-          <img
-            src="/logo.svg"
-            alt="Agency Logo"
-            className={styles.logo}
-          />
-          <div className={styles.line}></div>
-          <nav className={styles.navbar}>
-            <ul className={styles.navlist}>
-              <li>
+      <header className={styles.header}>
+        <img
+          src="/logo.svg"
+          alt="Agency Logo"
+          className={styles.logo}
+        />
+        <div className={styles.line}></div>
+        <nav className={styles.navbar}>
+          <ul className={styles.navlist}>
+            <li>
+              <a href="#">
                 <span className={styles["navlink-prefix"]}>00</span>
                 <span className={styles.navlink}>Home</span>
-              </li>
-              <li>
+              </a>
+            </li>
+            <li>
+              <a href="#">
                 <span className={styles["navlink-prefix"]}>01</span>
                 <span className={styles.navlink}>Destination</span>
-              </li>
-              <li>
+              </a>
+            </li>
+            <li>
+              <a href="#">
                 <span className={styles["navlink-prefix"]}>02</span>
                 <span className={styles.navlink}>Crew</span>
-              </li>
-              <li>
+              </a>
+            </li>
+            <li>
+              <a href="#">
                 <span className={styles["navlink-prefix"]}>03</span>
                 <span className={styles.navlink}>Technology</span>
-              </li>
-            </ul>
-          </nav>
-        </header>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className={styles.main}>
+        <div className={styles["text-container"]}>
+          <span className={styles["subheading-1"]}>
+            So, you want to travel to
+          </span>
+          <h1 className={styles["heading-1"]}>space</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as
+            well genuinely go to outer space and not hover kind of
+            on the edge of it. Well sit back, and relax because
+            we’ll give you a truly out of this world experience!
+          </p>
+        </div>
+        <a className={styles.cta}>Explore</a>
       </main>
     </>
   );
