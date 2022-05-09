a5620e2bf68164b8f5ddc3cb3b36eebae957f2d9
Zero0k <robykaurin@gmail.com>
2022-05-10 00:18:25 +0200

    Added active classes for navs, and tidied up /dest

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import styles from "../../styles/Navbar.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar: NextPage = () => {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
     <header className={styles.header}>
       <img
@@ -13,30 +16,54 @@ const Navbar: NextPage = () => {
       <div className={styles.line}></div>
       <nav className={styles.navbar}>
         <ul className={styles.navlist}>
-          <Link href="/">
-            <a>
-              <span className={styles["navlink-prefix"]}>00</span>
-              <span className={styles.navlink}>Home</span>
-            </a>
-          </Link>
-          <Link href="/destination">
-            <a>
-              <span className={styles["navlink-prefix"]}>01</span>
-              <span className={styles.navlink}>Destination</span>
-            </a>
-          </Link>
-          <Link href="/crew">
-            <a>
-              <span className={styles["navlink-prefix"]}>02</span>
-              <span className={styles.navlink}>Crew</span>
-            </a>
-          </Link>
-          <Link href="/technology">
-            <a>
-              <span className={styles["navlink-prefix"]}>03</span>
-              <span className={styles.navlink}>Technology</span>
-            </a>
-          </Link>
+          <li
+            className={`${
+              router.pathname == "/" ? styles.active : ""
+            }`}
+          >
+            <Link href="/">
+              <a>
+                <span className={styles["navlink-prefix"]}>00</span>
+                <span className={`${styles.navlink}`}>Home</span>
+              </a>
+            </Link>
+          </li>
+          <li
+            className={`${
+              router.pathname == "/destination" ? styles.active : ""
+            }`}
+          >
+            <Link href="/destination">
+              <a>
+                <span className={styles["navlink-prefix"]}>01</span>
+                <span className={styles.navlink}>Destination</span>
+              </a>
+            </Link>
+          </li>
+          <li
+            className={`${
+              router.pathname == "/crew" ? styles.active : ""
+            }`}
+          >
+            <Link href="/crew">
+              <a>
+                <span className={styles["navlink-prefix"]}>02</span>
+                <span className={styles.navlink}>Crew</span>
+              </a>
+            </Link>
+          </li>
+          <li
+            className={`${
+              router.pathname == "/technology" ? styles.active : ""
+            }`}
+          >
+            <Link href="/technology">
+              <a>
+                <span className={styles["navlink-prefix"]}>03</span>
+                <span className={styles.navlink}>Technology</span>
+              </a>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
--- pages/destination/index.tsx
+++ pages/destination/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useState } from "react";
 import styles from "../../styles/Destination.module.css";
 import data from "../../public/data.json";
+import Head from "next/head";
 
 const Destination: NextPage = () => {
   const [planet, setPlanet] = useState(0);
@@ -10,49 +11,59 @@ const Destination: NextPage = () => {
     .split("-")[1]
     .split(".")[0];
 
-  const tabs = data.destinations.map((planet, index) => (
+  const tabs = data.destinations.map((element, index) => (
     <button
-      className={styles["tab-button"]}
+      className={`${styles["tab-button"]} ${
+        planet === index ? styles["active-tab"] : ""
+      }`}
       onClick={setPlanet.bind(null, index)}
     >
-      {planet.name}
+      {element.name}
     </button>
   ));
 
   return (
-    <main className={styles.main}>
-      <div className={styles.grid}>
-        <span className={styles["subheading-1"]}>
-          Pick your destination
-        </span>
-        <img
-          src={data.destinations[planet].images.png}
-          alt={altText}
-          className={styles.image}
-        />
-        <div className={styles["text-container"]}>
-          <div className={styles.subnav}>{tabs}</div>
-          <h1 className={styles["heading-1"]}>
-            {data.destinations[planet].name}
-          </h1>
-          <p>{data.destinations[planet].description}</p>
-          <div className={styles["info-container"]}>
-            <div className={styles["info-title"]}>
-              Avg. distance
-            </div>
-            <div className={styles["info"]}>
-              {data.destinations[planet].distance}
-            </div>
-            <div className={styles["info-title"]}>
-              Est. travel time
-            </div>
-            <div className={styles["info"]}>
-              {data.destinations[planet].travel}
+    <>
+      <Head>
+        <title>Destination | Space Tourism</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          <div>
+            <span className={styles["subheading-prefix"]}>01</span>
+            <span className={styles["subheading-1"]}>
+              Pick your destination
+            </span>
+          </div>
+          <img
+            src={data.destinations[planet].images.png}
+            alt={altText}
+            className={styles.image}
+          />
+          <div className={styles["text-container"]}>
+            <div className={styles.subnav}>{tabs}</div>
+            <h1 className={styles["heading-1"]}>
+              {data.destinations[planet].name}
+            </h1>
+            <p>{data.destinations[planet].description}</p>
+            <div className={styles["info-container"]}>
+              <div className={styles["info-title"]}>
+                Avg. distance
+              </div>
+              <div className={styles["info"]}>
+                {data.destinations[planet].distance}
+              </div>
+              <div className={styles["info-title"]}>
+                Est. travel time
+              </div>
+              <div className={styles["info"]}>
+                {data.destinations[planet].travel}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
